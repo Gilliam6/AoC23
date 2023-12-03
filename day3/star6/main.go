4073c523b7163a12d6e32331b5e48d9fc73ad41c
Gilliam6 <wizzzardoz@icloud.com>
2023-12-04 01:50:34 +0400

    day 3

--- day3/star6/main.go
+++ day3/star6/main.go
@@ -7,42 +7,50 @@ import (
 	"unicode"
 )
 
-var arrayNumPos = make(map[int]bool)
+// var arrayNumPos = make(map[int]bool)
 var arraySymbolRange = make(map[int]bool)
 
 func main() {
 	var res int
 	field := os.Args[1]
 	twoDimArr := strings.Split(field, "\n")
-	size := len(twoDimArr[0])
+	//size := len(twoDimArr[0])
 
 	for y, line := range twoDimArr {
 		for x, char := range line {
-			if !unicode.IsDigit(char) && char != '.' {
-				checkField(twoDimArr, x, y, len(line))
+			if char == '*' {
+				res += checkField(twoDimArr, x, y, len(line))
 			}
-		}
-	}
-	for k, v := range arraySymbolRange {
-		if v == true && unicode.IsDigit(rune(twoDimArr[k/size][k%size])) {
-			res += findNum(twoDimArr, k, size)
 		}
 	}
 
 	fmt.Println(res)
 }
 
-func checkField(field []string, x int, y int, size int) {
+func checkField(field []string, x int, y int, size int) (res int) {
 	for i := -1; i < 2; i++ {
 		for n := 1; n > -2; n-- {
 			if x+n < size && x+n > -1 && y+i > -1 && y+i < len(field) {
-				if n == 0 && i == 0 {
+				if n == 0 && i == 0 && !unicode.IsDigit(rune(field[y+i][x+n])) {
 					continue
 				}
 				arraySymbolRange[size*(y+i)+x+n] = true
 			}
 		}
 	}
+	count := 0
+	res = 1
+	for k, _ := range arraySymbolRange {
+		if unicode.IsDigit(rune(field[k/size][k%size])) && arraySymbolRange[k] == true {
+			num := findNum(field, k, size)
+			count++
+			res *= num
+		}
+	}
+	if count != 2 {
+		return 0
+	}
+	return res
 }
 
 func findNum(twoDimArr []string, pos int, size int) (number int) {
